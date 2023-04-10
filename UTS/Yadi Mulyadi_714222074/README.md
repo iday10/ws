75eb6a6b49ea3565dc156bb6262cddc43db703c4
iday10 <idaymulyadi10@gmail.com>
2023-04-11 06:45:28 +0700

    Post UTS - Tahap1 (Belum Usai)

---
+++ UTS/Yadi Mulyadi_714222074/README.md
@@ -0,0 +1,15 @@
+UTS PEMROGRAMAN III
+Yadi Mulyadi - 714222074
+
+## Notes ##
+Frontend:
+https://tailwindcomponents.com/component/table-with-filter-and-search
+
+API Public:
+https://pokeapi.co/api/v2/pokemon/ditto
+
+Data Variable:
+image.png https://imgur.com/a/te47ox5
+
+
+
---
+++ UTS/Yadi Mulyadi_714222074/dayiday.js
@@ -0,0 +1,9 @@
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("https://pokeapi.co/api/v2/pokemon/ditto", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
